c194a2ab5fb9f4dc9d4cf8b0389e984d56ab13ce
Leonardo Amet <leonardo.amet@gmail.com>
2019-12-19 18:51:04 -0300

    first commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+            <div>
+                <example-component></example-component>
+            </div>
+            <div>            
+                <line-chart-component></line-chart-component>
+            </div>
+            
+@endsection            
